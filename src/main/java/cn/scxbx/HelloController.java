bd8968202ba6518238da0d608638946d3368f2ec
scxbx <761189437@qq.com>
2022-06-19 18:33:06 +0800

    mvc

--- src/main/java/cn/scxbx/HelloController.java
+++ src/main/java/cn/scxbx/HelloController.java
@@ -4,13 +4,16 @@ package cn.scxbx;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
 
 @Controller
 public class HelloController {
     @RequestMapping("/test")
     public String test(Model model) {
-        model.addAttribute("msg", "hello");
+        model.addAttribute("msg", "<h1>hello</h1>");
+        model.addAttribute("users", Arrays.asList("xie", "bang", "xian"));
         return "test";
     }
 
